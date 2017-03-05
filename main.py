0461d2bb1afb62fdae1e6d168119d9cc731e034d
sam <melaine.samy@gmail.com>
2017-03-05 17:20:18 +0100

    adding argv to main

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import json
-from take_action import TakeAction
+import sys
 from define_action import DefineAction
+from take_action import TakeAction
 
 if __name__=="__main__":
-    with open('example_data/example_9.json') as data_file:
+
+    try:
+        json_file = sys.argv[1]
+        laws_folder = sys.argv[2]
+    except:
+        json_file = 'example_data/example_9.json' 
+        laws_folder = "example_data/law_data/"
+
+    with open(json_file) as data_file:
         data = json.load(data_file)
 
-    define_it = DefineAction(path_origin="example_data/law_data/")
+    define_it = DefineAction(path_origin=laws_folder)
     context = define_it.defineAction(data)
 
     take_it = TakeAction(json_path='in.json')
